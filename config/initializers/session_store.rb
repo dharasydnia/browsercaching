3f191df86542bd812de1bca03632ec60e61485bd
dharasydnia <djoshi@sydnia.com>
2013-05-11 11:24:13 +0530

    initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+Browsercaching::Application.config.session_store :cookie_store, key: '_browsercaching_session'
+
+Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 2.minutes
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Browsercaching::Application.config.session_store :active_record_store
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Browsercaching::Application.routes.draw do
+  resources :contacts
+  root :to => 'contacts#index'
+end
