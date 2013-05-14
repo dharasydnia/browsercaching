496fadb412e9f539d9813f63e625aa8d526f1343
dharasydnia <djoshi@sydnia.com>
2013-05-14 19:23:46 +0530

    removed aftersave and destroy call from model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,8 @@
 class Contact < ActiveRecord::Base
   attr_accessible :email, :name
 
-  after_save    :expire_contact_all_cache
-  after_destroy :expire_contact_all_cache
+#  after_save    :expire_contact_all_cache
+#  after_destroy :expire_contact_all_cache
 
   def self.all_cached
     Rails.cache.fetch('Contact.all') { all }
