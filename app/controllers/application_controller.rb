003643cc05cf7b97d7397157ad966ecc0295e498
dharasydnia <djoshi@sydnia.com>
2013-05-15 11:41:16 +0530

    added cacahe_briefly method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def timestamp
+    cache_briefly
     render :text => Time.now
+  end
+
+  def cache_briefly
+    return unless Rails.env == "production"
+    expires_in 5.minutes, :public => true
   end
 end
